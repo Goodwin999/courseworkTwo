9aa187c706a9ed792106f9a9c31ba4c43c100417
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2024-02-28 19:29:37 +0300

    init

---
+++ src/main/java/com/example/courseworkTwo/CourseworkTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseworkTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseworkTwoApplication.class, args);
+	}
+
+}
